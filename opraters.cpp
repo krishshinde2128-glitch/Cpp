3f0ccb7014328964cd2ec0d4321e95a00458dee5
krish shinde <krishshinde@krishs-MacBook-Air.local>
2026-01-07 10:43:38 +0530

    while and opeaters

---
+++ opraters.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    char op;
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+    cout << "Enter an operator (+, -, *, /, ** ): ";
+    cin >> op;
+    switch(op) {
+        case '+':
+            cout << "Result: " << a + b << endl;
+            break;
+        case '-':
+            cout << "Result: " << a - b << endl;
+            break;
+        case '*':
+            cout << "Result: " << a * b << endl;
+            break;
+        case '**':
+            cout << "Result: " << (a * a) + (b * b) << endl;
+            break;
+        case '/':
+            if (b != 0) {
+                cout << "Result: " << a / b << endl;
+            } else {
+                cout << "Error: Division by zero!" << endl;
+            }
+            break;
+        default:
+            cout << "Invalid operator! Please use one of +, -, *, /." << endl;
+    }
+    return 0;
+}
